741358f8fa038290b06978e5d77ec939e1dc2154
Lei Hsu <ge159951@gmail.com>
2024-02-22 01:50:12 +0800

    fix: fix env

--- src/sanity.ts
+++ src/sanity.ts
@@ -3,7 +3,5 @@ import { createClient } from '@sanity/client';
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_API_KEY || process.env.API_KEY || '',
   dataset: 'production',
-  useCdn: true, // set to `false` to bypass the edge cache
-  // apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
-  // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
+  useCdn: true,
 });
