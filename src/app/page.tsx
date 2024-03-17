11764cd7e1abecd36da9b020cca49f64d8767fda
Lei Hsu <ge159951@gmail.com>
2024-03-18 01:22:07 +0800

    feat: add ga4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,17 +47,49 @@ export default function Campaign() {
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `<!-- Google Tag Manager -->
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_API_KEY || process.env.API_KEY || ''}');
-    <!-- End Google Tag Manager -->`,
+                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','${
+                    process.env.NEXT_PUBLIC_GTM_ID ||
+                    process.env.GTM_ID ||
+                    ""
+                  }');
+                  <!-- End Google Tag Manager -->`,
+        }}
+      />
+      <Script
+        id="gtag-main"
+        src={`https://www.googletagmanager.com/gtag/js?id=${
+          process.env.NEXT_PUBLIC_GA4_ID || process.env.GA4_ID || ""
+        }`}
+        strategy="afterInteractive"
+      />
+
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${
+                    process.env.NEXT_PUBLIC_GA4_ID ||
+                    process.env.GA4_ID ||
+                    ""
+                  }');
+                `,
         }}
       />
       <noscript>
         <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_API_KEY || process.env.API_KEY || ''}`}
+          src={`https://www.googletagmanager.com/ns.html?id=${
+            process.env.NEXT_PUBLIC_GTM_ID ||
+            process.env.GTM_ID ||
+            ""
+          }`}
           height="0"
           width="0"
           style={{ display: "none", visibility: "hidden" }}
